d4a23fa7e232a9b7118f750339708622d257d891
Ilya Vladimirskiy <ilya.vladimirskiy@smartfrog.com>
2017-12-02 23:16:28 +0100

    database connection

--- database/db_schema.sql
+++ database/db_schema.sql
@@ -1,4 +1,6 @@
-﻿DROP TABLE IF EXISTS user_language CASCADE;
+﻿CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+DROP TABLE IF EXISTS user_language CASCADE;
 CREATE TABLE user_language(
 l_id serial primary key,
 l_language text
@@ -22,35 +24,42 @@ INSERT INTO record_session_category (c_category)
 VALUES
 ('Family'),
 ('Friends'),
-('French'),
-('Spanish'),
-('Arabic'),
-('Russian');
+('Business');
 
 DROP TABLE IF EXISTS user_account CASCADE;
 CREATE TABLE user_account(
 u_id serial primary key,
-u_uuid UUID,
-u_firstname text,
-u_lastname text,
+u_uuid UUID NOT NULL,
+u_firstname text NOT NULL,
+u_lastname text NOT NULL,
 u_native_language integer references user_language(l_id),
 u_country text,
 u_address text
 );
 CREATE UNIQUE INDEX name ON user_account (u_uuid);
 
-DROP TABLE IF EXISTS user_word CASCADE;
+INSERT INTO user_account (u_uuid, u_firstname, u_lastname)
+VALUES
+('36ff545d-ca5a-4855-985b-eda712781efb','John', 'Miller'),
+(uuid_generate_v4(), 'Merry', 'Shelly'),
+(uuid_generate_v4(), 'Fyodor', 'Raskolnikow');
+
+
+DROP TABLE IF EXISTS user_record_sessions CASCADE;
 CREATE TABLE user_record_sessions(
 r_id serial primary key,
-r_language integer references user_language(l_id),
-r_
+r_created timestamp,
+r_language integer references user_language(l_id)
+);
 
 
-DROP TABLE IF EXISTS word CASCADE;
+DROP TABLE IF EXISTS user_words CASCADE;
 CREATE TABLE user_words(
 w_id serial primary key,
-w_language text
+w_user_id integer references user_account(u_id),
+w_language text,
 w_word text,
-
+w_count integer
 );
+
 
